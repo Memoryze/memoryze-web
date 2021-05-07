d333f185cdbeec9e848e2c3805008a7a637b82c8
alishalawani <alishalawani@gmail.com>
2021-05-07 01:13:26 -0500

    displayed the features

--- src/Components/LandingView/LandingView.js
+++ src/Components/LandingView/LandingView.js
@@ -1,32 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../Header/Header';
 import semicircleBackground from './assets/semicircleBackground.png';
 import manStudying from './assets/manStudying.png';
+import { featuresData } from './data';
 import './LandingView.css';
 
 function LandingView(props) {
-    return (
-			<div className='home-container'>
-				<img
-					src={semicircleBackground}
-					alt='semicircle background'
-					className='semicircle-background'
-				/>
-				<Header/>
-				<div className='pitch-container'>
-					<div className='pitch'>
-						<h3>Audio learning for wherever, whenever</h3>
-						<p>
-							"The other side of the coin is, however, that there is a direct
-							relation between the system concepts and the progress of the
-							operations research"
-						</p>
-						<a href='/app-link' className="get-the-app-btn" >Get the app!</a>
-					</div>
-					<img src={manStudying} alt='man-studying' className='man-studying' />
+	const [features, setFeatures] = useState(featuresData);
+	const mappedFeatures = features.map(({ name, description, image }) => {
+		return (
+			<div className='feature-contents'>
+				<img src={image} alt={name + 'icon'} className='feature-image' />
+				<div className='feature-texts'>
+					<h5 className='feature-name'>{name}</h5>
+					<p className='feature-description'>{description}</p>
 				</div>
 			</div>
 		);
+	});
+	return (
+		<div className='home-container'>
+			<img
+				src={semicircleBackground}
+				alt='semicircle background'
+				className='semicircle-background'
+			/>
+			<Header />
+			<div className='section-container'>
+				<div className='pitch'>
+					<h3>Audio learning for wherever, whenever</h3>
+					<p>
+						"The other side of the coin is, however, that there is a direct
+						relation between the system concepts and the progress of the
+						operations research"
+					</p>
+					<a href='/app-link' className='get-the-app-btn'>
+						Get the app!
+					</a>
+				</div>
+				<img src={manStudying} alt='man-studying' className='man-studying' />
+			</div>
+			<div className='section-container'>
+				<div className='features'>
+					<h4 className='features-heading'>Features</h4>
+					<div className='feature-contents-container'>{mappedFeatures}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default LandingView;
---
+++ src/Components/LandingView/data.js
@@ -0,0 +1,43 @@
+import Topics from './assets/Topics.png';
+import Educators from './assets/Educators.png';
+import Community from './assets/Community.png';
+import Resources from './assets/Resources.png';
+import OfflineUse from './assets/OfflineUse.png';
+import Quizzes from './assets/Quizzes.png';
+
+export const featuresData = [
+	{
+		name: 'Topics',
+		description:
+			'Learn anything, ranging from languages to psychology hacks to design. Audio is limitless.',
+		image: Topics,
+	},
+	{
+		name: 'Quizzes',
+		description:
+			'Get quizzed at the end of each lesson to check how well you paid attention.',
+		image: Quizzes,
+	},
+	{
+		name: 'Educators',
+		description:
+			'Courses are created and taught by vetted educators from across the globe.',
+		image: Educators,
+	},
+	{
+		name: 'Community',
+		description: 'Meet other learners with the same interest as you.',
+		image: Community,
+	},
+	{
+		name: 'Offline Use',
+		description:
+			"Download lessons so you can listen to them even when you're out of data.",
+		image: OfflineUse,
+	},
+	{
+		name: 'Resources',
+		description: 'Courses come with short PDF materials as learning aids.',
+		image: Resources,
+	},
+];
