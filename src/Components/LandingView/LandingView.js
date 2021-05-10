eff4fee027c2b47e05801499ce93d62998df984a
alishalawani <alishalawani@gmail.com>
2021-05-09 22:17:33 -0500

    created an unfinished structure for the memoji section

--- src/Components/LandingView/LandingView.js
+++ src/Components/LandingView/LandingView.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import semicircleBackground from '../../assets/semicircleBackground.png';
 import manStudying from '../../assets/manStudying.png';
 import PhoneMockup from '../../assets/PhoneMockup.png';
+import Rockstar from '../../assets/Rockstar.png';
+import dreadedGuy from '../../assets/dreadedGuy.png';
 import { featuresData } from './data';
 import './LandingView.css';
 
@@ -46,7 +48,46 @@ function LandingView(props) {
 					<h4 className='features-heading'>Features</h4>
 					<div className='feature-contents-container'>{mappedFeatures}</div>
 				</div>
-				<img src={PhoneMockup} alt='phone mockup' className="phone-mockup"/>
+				<img src={PhoneMockup} alt='phone mockup' className='phone-mockup' />
+			</section>
+			<h4 className='section-heading'>Learn for the sake of learning</h4>
+			<section className='section-container rock-star-section'>
+				<img src={Rockstar} alt='rock star' className='rock-star' />
+				<div className='rock-star-text'>
+					<h5 className='rock-star-heading'>Learn like a Rockstar</h5>
+					<p className='rock-star-description'>{`"That is to say the lack of knowledge of impact of the application interface has common features with The Availability of Professional Evaluation" (Millard Hewitt in The Book of the Well-Known Practice) On the other hand, elements of the essence directly changes the principles of the first-class package.`}</p>
+				</div>
+			</section>
+			<h4 className='section-heading'>
+				Hundreds of people have signup up for early access
+			</h4>
+			<p className='section-subheading'>Here's what some of them had to say</p>
+			<section className='section-container memoji-section'>
+				<img
+					src={dreadedGuy}
+					alt='memoji and comment'
+					className='memoji-person'
+				/>
+				<img
+					src={dreadedGuy}
+					alt='memoji and comment'
+					className='memoji-person'
+				/>
+				<img
+					src={dreadedGuy}
+					alt='memoji and comment'
+					className='memoji-person'
+				/>
+				<img
+					src={dreadedGuy}
+					alt='memoji and comment'
+					className='memoji-person'
+				/>
+				<img
+					src={dreadedGuy}
+					alt='memoji and comment'
+					className='memoji-person'
+				/>
 			</section>
 		</main>
 	);
