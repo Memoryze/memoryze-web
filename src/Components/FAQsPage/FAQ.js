bd46ee8af1375fffde0ab3588d890bcf23a02df8
alishalawani <alishalawani@gmail.com>
2021-06-11 15:58:45 -0500

    some ne changes

--- src/Components/FAQsPage/FAQ.js
+++ src/Components/FAQsPage/FAQ.js
@@ -5,13 +5,21 @@ import { RiArrowUpSLine, RiArrowDownSLine } from 'react-icons/ri';
 function FAQ({ question, answer, id }) {
 	const [open, setOpen] = useState(false);
 	return (
-		<div className="faq-panel">
+		<div className='faq-panel'>
 			<div className='question-container'>
 				<h5 className='question'>{question}</h5>
 				{open ? (
-					<RiArrowUpSLine onClick={() => setOpen(false)} className='arrow' />
+					<RiArrowUpSLine
+						onClick={() => setOpen(false)}
+						className='arrow'
+						size={20}
+					/>
 				) : (
-					<RiArrowDownSLine onClick={() => setOpen(true)} className='arrow' />
+					<RiArrowDownSLine
+						onClick={() => setOpen(true)}
+						className='arrow'
+						size={20}
+					/>
 				)}
 			</div>
 			<Collapse in={open}>
