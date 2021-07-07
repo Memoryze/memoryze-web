f9fcb4dca69f3d13e4f57ab9e473bc12553938ac
alishalawani <alishalawani@gmail.com>
2021-07-07 13:14:09 -0500

    added animation on scroll to the landing page

--- src/Components/LandingView/LandingView.js
+++ src/Components/LandingView/LandingView.js
@@ -5,6 +5,8 @@ import PhoneMockup from '../../assets/landingPage/PhoneMockup.png';
 import Rockstar from '../../assets/landingPage/Rockstar.png';
 
 import { featuresData, memojies } from './data';
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
 import './LandingView.css';
 
 function LandingView(props) {
@@ -27,58 +29,68 @@ function LandingView(props) {
 				alt='semicircle background'
 				className='semicircle-background'
 			/>
-			<section className='section-container landing-section'>
-				<div className='pitch'>
-					<h3 className='pitch-heading'>
-						Audio learning for wherever, whenever
-					</h3>
-					<p className='pitch-paragraph'>
-						Learn something new everyday. The best part is you don't have to put
-						a pause on whatever you're doing to learn. Just select what you want
-						to learn, connect your headphones and press play!
-					</p>
-					<a href='/wait-list' className='get-the-app-btn'>
-						Get early access!
-					</a>
-				</div>
-				<img src={manStudying} alt='man-studying' className='top-image' />
-			</section>
-			<section className='section-container'>
-				<div className='features'>
-					<h4 className='features-heading'>Features</h4>
-					<div className='feature-contents-container'>{mappedFeatures}</div>
-				</div>
-				<img src={PhoneMockup} alt='phone mockup' className='phone-mockup' />
-			</section>
-			<h4 className='section-heading'>Learn for the sake of learning</h4>
-			<section className='section-container rock-star-section'>
-				<img src={Rockstar} alt='rock star' className='rock-star' />
-				<div className='rock-star-text'>
-					<h5 className='rock-star-heading'>Live and learn</h5>
-					<p className='rock-star-description'>{`Memoryze is a learning app designed to fit right in with your busy (and maybe not-so-busy) schedules. Audio learning takes the bore out of your daily routine and makes it 10x times more productive. 
+			<Zoom>
+				<section className='section-container landing-section'>
+					<div className='pitch'>
+						<h3 className='pitch-heading'>
+							Audio learning for wherever, whenever
+						</h3>
+						<p className='pitch-paragraph'>
+							Learn something new everyday. The best part is you don't have to
+							put a pause on whatever you're doing to learn. Just select what
+							you want to learn, connect your headphones and press play!
+						</p>
+						<a href='/wait-list' className='get-the-app-btn'>
+							Get early access!
+						</a>
+					</div>
+					<img src={manStudying} alt='man-studying' className='top-image' />
+				</section>
+			</Zoom>
+			<Zoom>
+				<section className='section-container'>
+					<div className='features'>
+						<h4 className='features-heading'>Features</h4>
+						<div className='feature-contents-container'>{mappedFeatures}</div>
+					</div>
+					<img src={PhoneMockup} alt='phone mockup' className='phone-mockup' />
+				</section>
+			</Zoom>
+			<Zoom>
+				<h4 className='section-heading'>Learn for the sake of learning</h4>
+				<section className='section-container rock-star-section'>
+					<img src={Rockstar} alt='rock star' className='rock-star' />
+					<div className='rock-star-text'>
+						<h5 className='rock-star-heading'>Live and learn</h5>
+						<p className='rock-star-description'>{`Memoryze is a learning app designed to fit right in with your busy (and maybe not-so-busy) schedules. Audio learning takes the bore out of your daily routine and makes it 10x times more productive. 
 					You get to learn whatever you feel like learning at your own pace, and it requires little effort - just listen, learn and have fun.`}</p>
-					<a href='/wait-list' className='get-the-app-btn'>
-						Get early access!
-					</a>
-				</div>
-			</section>
-			<h4 className='section-heading'>
-				Hundreds of people have signup up for early access
-			</h4>
-			<p className='section-subheading'>Here's what some of them had to say</p>
-			<section className='section-container memoji-section'>
-				{memojies.map(({ name, image, comment }, i) => {
-					return (
-						<div className='memoji-card' key={i + name}>
-							<img src={image} alt='a memoji' className='memoji' />
-							<div className='memoji-texts-container'>
-								<p className='memoji-name'>{name}</p>
-								<p className='memoji-comment'>{comment}</p>
+						<a href='/wait-list' className='get-the-app-btn'>
+							Get early access!
+						</a>
+					</div>
+				</section>
+			</Zoom>
+			<Fade>
+				<h4 className='section-heading'>
+					Hundreds of people have signup up for early access
+				</h4>
+				<p className='section-subheading'>
+					Here's what some of them had to say
+				</p>
+				<section className='section-container memoji-section'>
+					{memojies.map(({ name, image, comment }, i) => {
+						return (
+							<div className='memoji-card' key={i + name}>
+								<img src={image} alt='a memoji' className='memoji' />
+								<div className='memoji-texts-container'>
+									<p className='memoji-name'>{name}</p>
+									<p className='memoji-comment'>{comment}</p>
+								</div>
 							</div>
-						</div>
-					);
-				})}
-			</section>
+						);
+					})}
+				</section>
+			</Fade>
 		</main>
 	);
 }
