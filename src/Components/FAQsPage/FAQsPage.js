e42acb0c5163043e9196ba035ed2b7dd370b31bb
alishalawani <alishalawani@gmail.com>
2021-07-11 20:48:04 -0500

    added the fade to the educators page

--- src/Components/FAQsPage/FAQsPage.js
+++ src/Components/FAQsPage/FAQsPage.js
@@ -4,6 +4,8 @@ import FAQ from './FAQ';
 import './FAQsPage.css';
 import semicircleBackground from '../../assets/landingPage/semicircleBackground.png';
 import FAQs from '../../assets/FAQs.png';
+import Fade from 'react-reveal/Fade';
+
 function FAQsPage(props) {
 	const [faqs] = useState({ educators: educators, learners: learners });
 	const [forEducators, setForEducators] = useState(true);
@@ -15,6 +17,7 @@ function FAQsPage(props) {
 	});
 	return (
 		<main className='home-container'>
+			<Fade>
 			<img
 				src={semicircleBackground}
 				alt='semi circle background'
@@ -22,29 +25,32 @@ function FAQsPage(props) {
 			/>
 			<h1 className='faqs-heading'>Questions We Get Asked...A Lot</h1>
 			<img src={FAQs} alt='people chatting' className='faqs-image' />
-			<div className='btn-container'>
-				<button
-					className={
-						forEducators ? 'active-btn faq-btn' : 'inactive-btn faq-btn'
-					}
-					onClick={() => {
-						setForEducators(true);
-					}}>
-					By Educators
-				</button>
-				<button
-					className={
-						!forEducators ? 'active-btn faq-btn' : 'inactive-btn faq-btn'
-					}
-					onClick={() => {
-						setForEducators(false);
-					}}>
-					By Learners
-				</button>
-			</div>
-			<div className='faqs-container'>
-				{forEducators ? educatorsFAQs : learnersFAQs}
-			</div>
+			</Fade>
+			<Fade>
+				<div className='btn-container'>
+					<button
+						className={
+							forEducators ? 'active-btn faq-btn' : 'inactive-btn faq-btn'
+						}
+						onClick={() => {
+							setForEducators(true);
+						}}>
+						By Educators
+					</button>
+					<button
+						className={
+							!forEducators ? 'active-btn faq-btn' : 'inactive-btn faq-btn'
+						}
+						onClick={() => {
+							setForEducators(false);
+						}}>
+						By Learners
+					</button>
+				</div>
+				<div className='faqs-container'>
+					{forEducators ? educatorsFAQs : learnersFAQs}
+				</div>
+			</Fade>
 		</main>
 	);
 }
--- src/Components/ForEducators/ForEducators.js
+++ src/Components/ForEducators/ForEducators.js
@@ -3,6 +3,7 @@ import Rectangle from '../../assets/forEducators/Rectangle.png';
 import Presentation from '../../assets/forEducators/Presentation.png';
 import { perksData, processesData } from './data';
 import './ForEducators.css';
+import Fade from 'react-reveal/Fade';
 
 function ForEducators(props) {
 	const [perks] = useState(perksData);
@@ -17,8 +18,12 @@ function ForEducators(props) {
 				<div className='perk-texts'>
 					<h5 className='perk'>
 						<br></br>
-
-						{perk} {i === 0 && <p className='beta-icon' title="alpha feature - coming later">αlpha feature</p>}
+						{perk}{' '}
+						{i === 0 && (
+							<p className='beta-icon' title='alpha feature - coming later'>
+								αlpha feature
+							</p>
+						)}
 					</h5>
 					<p className='perk-description'>{description}</p>
 				</div>
@@ -49,38 +54,46 @@ function ForEducators(props) {
 
 	return (
 		<main className='home-container'>
-			<img
-				src={Rectangle}
-				alt='semicircle background'
-				className='semicircle-background'
-			/>
-			<section className='section-container landing-section'>
-				<div className='pitch'>
-					<h3 className='pitch-heading'>Become an Educator</h3>
-					<p className='pitch-paragraph'>
-						Do you have expert knowledge on a particular topic? If yes, Memoryze
-						is for you. Create fantastic audio courses and monetize your
-						knowledge. That's right! You are getting paid for being smart
-					</p>
-					<a href='/educators-get-started' className='get-started-btn'>
-						Get started
-					</a>
-				</div>
+			<Fade>
 				<img
-					src={Presentation}
-					alt='people making a presentation'
-					className='top-image'
+					src={Rectangle}
+					alt='semicircle background'
+					className='semicircle-background'
 				/>
-			</section>
+				<section className='section-container landing-section'>
+					<div className='pitch'>
+						<h3 className='pitch-heading'>Become an Educator</h3>
+						<p className='pitch-paragraph'>
+							Do you have expert knowledge on a particular topic? If yes,
+							Memoryze is for you. Create fantastic audio courses and monetize
+							your knowledge. That's right! You are getting paid for being smart
+						</p>
+						<a href='/educators-get-started' className='get-started-btn'>
+							Get started
+						</a>
+					</div>
+					<img
+						src={Presentation}
+						alt='people making a presentation'
+						className='top-image'
+					/>
+				</section>
+			</Fade>
+
 			<div className='sub-container'>
-				<section className='section-container perks'>
-					<h5 className='perks-heading'>What's in it for you?</h5>
-					<div className='perks-contents-container'>{mappedPerks}</div>
-				</section>
-				<section className='section-container create-share-earn'>
-					<h5 className='processes-heading'>Create, Share, Earn.</h5>
-					<div className='processes-contents-container'>{mappedProcesses}</div>
-				</section>
+				<Fade>
+					<section className='section-container perks'>
+						<h5 className='perks-heading'>What's in it for you?</h5>
+						<div className='perks-contents-container'>{mappedPerks}</div>
+					</section>
+				</Fade>
+					<section className='section-container create-share-earn'>
+						<h5 className='processes-heading'>Create, Share, Earn.</h5>
+						<div className='processes-contents-container'>
+							{mappedProcesses}
+						</div>
+					</section>
+				
 			</div>
 		</main>
 	);
